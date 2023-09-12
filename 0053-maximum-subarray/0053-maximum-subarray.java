dd0817e87facf2432e3997a12673e2e6b2bbbaa7
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-12 16:57:32 -0500

    Time: 1 ms (100%), Space: 57.8 MB (82.59%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,8 +5,7 @@ class Solution {
         //the algorthim says that at any given index, we need to see if the subarray emding at
         //that index is greater then, just the singular subarray of that index, if it is not, we
         //will just add the current index onto the subbaray or we can start a new array with that index.
-        ArrayList<Integer> maxSub=new ArrayList<>();
-        maxSub.add(nums[0]); //starting with just 1 elemnt subarray
+        
         int maxSum=nums[0];
         int currSum=nums[0];
         if(nums.length==1){
@@ -16,11 +15,8 @@ class Solution {
         for(int i=1; i<nums.length; i++){
             if(currSum+nums[i]>nums[i]){
                 currSum=currSum+nums[i];
-                maxSub.add(nums[i]);
             }
             else{
-                maxSub.clear();
-                maxSub.add(nums[i]);
                 currSum=nums[i];
             }
             maxSum=Math.max(currSum,maxSum);
