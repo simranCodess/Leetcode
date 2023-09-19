556b58398290ba2027307ac4c58e909b1592a1a8
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-19 15:19:52 -0500

    Time: 1 ms (99.96%), Space: 62.1 MB (6.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,16 @@
-import java.util.*;
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice=Integer.MAX_VALUE;
         int maxProfit=0;
+        int indexOfBuying=0;
+        int min=Integer.MAX_VALUE;
 
-        for(int n: prices){
-            if(n<minPrice){
-                minPrice=n;
+        for(int i=0; i<prices.length; i++){
+            if(prices[i]<min){
+                min=prices[i];
+                indexOfBuying=i;
             }
-            else if(n-minPrice>maxProfit){
-                maxProfit=n-minPrice;
+            else if(prices[i]-min>maxProfit&&i>indexOfBuying){
+                maxProfit=prices[i]-min;
             }
         }
         return maxProfit;
