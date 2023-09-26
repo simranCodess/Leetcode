4e2c6ce1ffb96eb3cc6ee45c4223e37054ee7650
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-26 12:59:11 -0500

    Time: 3 ms (73.96%), Space: 41.9 MB (74.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
+        if(s.length()==1||s.length()==0){
+            return true;
+        }
         int left=0;
         int right=s.length()-1;
         while(left<=right){
