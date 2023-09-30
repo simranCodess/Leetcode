1f8c86036cc967a9555b4409ef01d3da8ed5a434
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 17:32:05 -0500

    Time: 18 ms (6.59%), Space: 54.7 MB (6.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,54 @@
-import java.util.*;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //left multiple
-        int [] leftProduct=new int[nums.length];
-        int currLeft=1;
-        leftProduct[0]=currLeft;
-        for(int i=0; i<nums.length-1; i++){
-            currLeft*=nums[i];
-            leftProduct[i+1]=currLeft;
+        int [] answer=new int[nums.length];
+        //edge case
+        if(nums.length==0){
+            return answer;
         }
 
-        int[] rightProduct=new int[nums.length];
-        int currRight=1;
-        rightProduct[nums.length-1]=currRight;
-        for(int i=nums.length-1; i>0; i--){
-            currRight*=nums[i];
-            rightProduct[i-1]=currRight;
-        }
-
-        
+        /*brute force
         for(int i=0; i<nums.length; i++){
-            nums[i]=leftProduct[i]*rightProduct[i];
+            answer[i]=1;
         }
-        return nums;
+        for(int i=0; i<nums.length; i++){
+            int curr=nums[i];
+            for(int j=0; j<nums.length; j++){
+                if(j==i){
+                    continue;
+                }
+                answer[i]*=nums[j];
+            }
+        }
+        return answer;*/ //very slow solution, time complexity of O(n^2) and space complexity of O(n)
+
+        //optimal solution : use left and right product array and then multiply those
+        int [] leftProduct=new int[nums.length];
+        int [] rightProduct=new int[nums.length];
+        
+        leftProduct[0]=1;
+        rightProduct[nums.length-1]=1;
+
+        int left=1;
+        int right=1;
+        for(int i=1; i<nums.length; i++){
+            left*=nums[i-1];
+            leftProduct[i]=left;
+        }
+
+        for(int i=nums.length-2; i>=0; i--){
+            right*=nums[i+1];
+            rightProduct[i]=right;
+        }
+
+        System.out.println(Arrays.toString(rightProduct));
+                System.out.println(Arrays.toString(leftProduct));
+
+        for(int i=0; i<nums.length; i++){
+            answer[i]=leftProduct[i]*rightProduct[i];
+        }
+       
+
+        return answer;
+
     }
 }
