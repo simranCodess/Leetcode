a1346d6f5ec342c77b988c5c7df8ac1a1520b5e6
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 14:41:54 -0500

    Time: 15 ms (27.25%), Space: 44.1 MB (6.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -8,11 +8,27 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        //Brute force approach
+        /*Brute force approach
         char [] sArray=s.toCharArray();
         char [] tArray=t.toCharArray();
         Arrays.sort(sArray);
         Arrays.sort(tArray);
-        return Arrays.equals(sArray,tArray);
+        return Arrays.equals(sArray,tArray); //Time Complexity: O(n log n) Space Complexity: O(n)*/
+
+        //optimal approach: HashMap
+        HashMap<Character,Integer> myMap=new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            myMap.put(s.charAt(i),myMap.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0; i<t.length(); i++){
+            myMap.put(t.charAt(i),myMap.getOrDefault(t.charAt(i),0)-1);
+        }
+        for(int value : myMap.values()){
+            if(value!=0){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
