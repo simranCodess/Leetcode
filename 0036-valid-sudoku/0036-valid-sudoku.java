47d845cdecb1213d3f5faa2c3c8b63386cb2623e
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 17:50:48 -0500

    Time: 9 ms (38.14%), Space: 43.4 MB (50.1%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        //brute force solution: going to check if a cell is not '.' and then compare ti with row, col and grid
+        //optimal solution: going to check if a cell is not '.' and then compare ti with row, col and grid
         Set<String> set=new HashSet<>();
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
                 if(board[i][j]!='.'){
                     int boxNum=(i/3)*3+j/3;
                     if(!set.add("ROW" + i + board[i][j])||!set.add("COL"+ j+board[i][j])||!set.add("BoxNum"+boxNum+board[i][j])){
@@ -13,7 +13,8 @@ class Solution {
             }
         }
 
-        return true;
+        return true; //time complexity : O(n^2) and space complexity ~ O(81) which is constant so O(1)
+
 
     }
 }
