617d0f37ca64575cf57d8e57cd0b85b3804051c4
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 15:27:58 -0500

    Time: 840 ms (5%), Space: 46.8 MB (56.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,47 +1,51 @@
 import java.util.*;
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map=new HashMap<>();
-        List<List<String>> answer=new ArrayList<>();
-        for(int i=0; i<strs.length; i++){
-            String curr=strs[i];
-             char tempArray[] = curr.toCharArray();
-             Arrays.sort(tempArray);
-             String sortedWord=new String(tempArray);
-             List<String> myList=new ArrayList<>();
-            if(map.containsKey(sortedWord)){
-                myList=map.get(sortedWord);
-                myList.add(curr);
-                map.put(sortedWord,myList);
-            }
-            else{
-                myList.add(curr);
-                map.put(sortedWord,myList);
-            }
+        List<List<String>> result=new ArrayList<>();
+        //edge case
+        if(strs.length==0){
+            return result;
         }
 
-        for(List<String> currList: map.values()){
-            answer.add(currList);
+        //brute force method
+        for(int i=0; i<strs.length; i++){
+            String curr=strs[i];
+            List<String> currentList=new ArrayList<>();
+            boolean added=false;
+            for(List<String> itterator: result){
+                //in case a anagram for this word exists
+                if(isAnagram(itterator.get(0),curr)){
+                    itterator.add(curr);
+                    added=true;
+                    break;
+                }
+            }
+            if(!added){
+            //if it doesnt exist
+            currentList.add(curr);
+            result.add(currentList);
+            }
         }
-        return answer;
-        
+        return result; //time complexity of O(n*m^2) where n is length of strs and m is length of strings space complexity: O(1)
     }
-    public boolean isAnanagram(String a,String b){
-        HashMap<Character,Integer> myTable=new HashMap<>();
-        if(a.length()==0||b.length()==0){
+
+    public boolean isAnagram(String s, String t){
+         if(s.length()==0&&t.length()==0){
+            return true;
+        }
+        if(s.length()!=t.length()){
             return false;
         }
-        for(int i=0; i<a.length(); i++){
-            myTable.put(a.charAt(i),myTable.getOrDefault(a.charAt(i),0)+1);
+        int[] seq=new int[26];
+        for(int i=0; i<s.length(); i++){
+            seq[s.charAt(i)-'a']++;
+             seq[t.charAt(i)-'a']--;
         }
-        for(int i=0; i<b.length(); i++){
-            myTable.put(b.charAt(i),myTable.getOrDefault(b.charAt(i),0)-1);
-        }
-        for(int i: myTable.values()){
+        for(int i:seq){
             if(i!=0){
                 return false;
             }
         }
-        return true;
+        return true;  //time complexity of O(n) and space complexity of O(1)
     }
 }
