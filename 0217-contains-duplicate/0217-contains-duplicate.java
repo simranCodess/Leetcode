f9dab8a4e8e2c195fecab6cc62244f444578950f
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 14:23:45 -0500

    Time: 6 ms (99.58%), Space: 55.9 MB (44.08%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,20 @@
-import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length==0){
-            return false;
+        /*brute force approach
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
         }
-        Set<Integer> set=new HashSet<>();
-        for(int i : nums){
-            if(set.contains(i)){
+        return false; Time Complexity: O(n^2) Space Complexity: O(1)*/
+        //optimal approach : use hashset
+        HashSet<Integer> mySet=new HashSet<>();
+        for(int i:nums){
+            if(!mySet.add(i)){
                 return true;
             }
-            set.add(i);
         }
         return false;
     }
