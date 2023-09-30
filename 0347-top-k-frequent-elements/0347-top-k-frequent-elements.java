c738b1c230a8b42e09df5adb35d5e6d97447161d
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 17:08:41 -0500

    Time: 10 ms (94.95%), Space: 47.1 MB (94.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,45 +1,39 @@
+import java.util.*;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int [] answer=new int[k];
         //edge cases
         if(nums.length==0||k==0){
-            return new int[0];    
+            return answer;
         }
-        HashMap<Integer,Integer> freq=new HashMap<>();
-        List<Integer> bucket[] =new ArrayList[nums.length+1];
-
+        // optimal way using bucket, indexes as count numbers and fill the values that count upto that index
+        List<Integer> [] bucket=new ArrayList[nums.length+1];
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-
-        for(int value: freq.keySet()){
-            int frequency=freq.get(value);
-            if(bucket[frequency]==null){
-                List<Integer> myList=new ArrayList<>();
-                myList.add(value);
-                bucket[frequency]=myList;
-            }
-            else{
-                List<Integer> myList=bucket[frequency];
-                myList.add(value);
-                bucket[frequency]=myList;
-            }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+             Integer key = entry.getKey();
+             Integer value = entry.getValue();
+             if(bucket[value]==null){
+                 bucket[value]=new ArrayList<>();
+             }
+             bucket[value].add(key);
         }
-        int [] answer=new int[k];
         int index=0;
+       for(int i=bucket.length-1; i>=0 &&index<k; i--){
+          if(bucket[i]!=null){
+              List<Integer> curr=bucket[i];
+              for(int j=0; j<curr.size(); j++){
+                  answer[index++]=curr.get(j);
+                  if(index==k){
+                      break;
+                  }
+              }
+          }
+       }
 
-        for(int i=bucket.length-1; i>=0; i--){
-            if(bucket[i]!=null){
-                List<Integer> myList=bucket[i];
-                for(int value: myList){
-                    answer[index]=value;
-                    index++;
-                    if(index==k){
-                        return answer;
-                    }
-                }
-            }
-        }
-        
-        return new int[0];
+
+        return answer; //time complexity of O(n) as well as space complexity O(n)
     }
 }
