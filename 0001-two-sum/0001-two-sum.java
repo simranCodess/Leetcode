c921e5e0bec15ba44437825d53a3d2d56677d04a
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-09-30 14:59:13 -0500

    Time: 49 ms (17.32%), Space: 43 MB (99.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,19 @@
-import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] answer=new int [2];
-        HashMap <Integer,Integer> myMap=new HashMap<>();
-
-        for(int i=0; i<nums.length; i++){
-            int diff=target-nums[i];
-            if(myMap.containsKey(diff)){
-                answer[0]=myMap.get(diff);
-                answer[1]=i;
-            }
-            myMap.put(nums[i],i);
+        int [] result=new int[2];
+        //edge case
+        if(nums.length==0){
+            return result;
         }
-        return answer;
+        //Brute force approach
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    result[0]=i;
+                    result[1]=j;
+                }
+            }
+        }
+        return result;
     }
 }
