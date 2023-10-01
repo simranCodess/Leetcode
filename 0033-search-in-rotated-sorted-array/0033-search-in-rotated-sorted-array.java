b83803eed391f24945530ef7dc1b5a53aa329a98
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-10-01 14:42:34 -0500

    Time: 0 ms (100%), Space: 41 MB (36.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,10 +1,37 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
-                return i;
+        //using binary search to do this
+        int left=0;
+        int right=nums.length-1;
+        int index=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                index=mid;
+                break;
             }
+             if(nums[mid]>=nums[left]){
+                //sorted left half
+                //checking if index exists in this half
+                if(target>=nums[left]&&target<nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+                }
+                //checking if it exists in other half
+                else{
+                    if(target>nums[mid]&&target<=nums[right]){
+                        left=mid+1;;
+                    }
+                    else{
+                        right=mid-1;
+                    }
+                }
+            }
+            return index; //time complexity of O(log n) and space of O(1)
+            
         }
-        return -1;
+       
     }
-}
