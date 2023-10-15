d31f00eee9cb4bee4b2f0e445d4573d495a22544
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-10-15 15:27:19 -0500

    Time: 1 ms (42.87%), Space: 40.3 MB (89.06%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i=s.length()-1;
+        if(s.length()==1){
+            return 1;
+        }
+        while(i>=0){
+            if(Character.isAlphabetic(s.charAt(i))){
+                break;
+            }
+            i--;
+        }
+        System.out.println(i);
+        //now i have the index of the last alphabetic char
+        int answer=0;
+        while(i>=0&&Character.isAlphabetic(s.charAt(i))){
+            i--;
+            answer++;
+        }
+        return answer;
+    }
+}
