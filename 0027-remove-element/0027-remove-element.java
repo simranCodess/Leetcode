deac980349c104c93e7c0893340dc542eb49af35
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-10-17 18:20:44 -0500

    Time: 0 ms (100%), Space: 41.3 MB (8.61%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index=0;
-       //remove the values in nums and shift elemnts 
-       for(int i=0; i<nums.length; i++){
-           if(nums[i]!=val){
-               nums[index]=nums[i];
-               index++;
-           }
-       } 
-       return index;
+        int leftPtr=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[leftPtr]=nums[i];
+                leftPtr++;
+            }
+        }
+        return leftPtr;
     }
 }
