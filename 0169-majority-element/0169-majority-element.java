8001af4466196072e86bc542322c0030d4d13413
Simran Kaur <127698711+simranCodess@users.noreply.github.com>
2023-10-19 20:08:25 -0500

    Time: 2 ms (68.3%), Space: 49.1 MB (5.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,20 @@
-import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-       if(nums.length==0){
-           return 0;
-       }
+        int majority=nums[0];
+        int count=1;
 
-       int majorityElement=nums[0];
-       int count=1;
-
-       for(int i=1; i<nums.length; i++){
-           if(nums[i]==majorityElement){
-               count++;
-           }
-           else{
-               count--;
-           }
-           if(count==0){
-               majorityElement=nums[i];
-               count=count+1;
-           }
-       }
-        return majorityElement;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]!=majority){
+                count--;
+            }
+            if(nums[i]==majority){
+                count++;
+            }
+            if(count<0){
+                majority=nums[i];
+                count=1;       
+            }
+        }
+        return majority;
     }
 }
